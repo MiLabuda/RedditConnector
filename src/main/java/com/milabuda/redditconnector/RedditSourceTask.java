12ecccfac4d24c446c8b5dcef726f8746d9457ce
Michał Łabuda <mlabuda544@gmail.com>
2024-12-29 19:44:30 +0100

    Big cleanup and added comments for main poll() method

--- src/main/java/com/milabuda/redditconnector/RedditSourceTask.java
+++ src/main/java/com/milabuda/redditconnector/RedditSourceTask.java
@@ -1,46 +1,48 @@
 package com.milabuda.redditconnector;
 
+import com.milabuda.redditconnector.api.client.CommentManager;
 import com.milabuda.redditconnector.api.client.PostManager;
-import com.milabuda.redditconnector.api.model.Envelope;
+import com.milabuda.redditconnector.api.model.Comment;
 import com.milabuda.redditconnector.api.model.Listing;
 import com.milabuda.redditconnector.api.model.Post;
 import com.milabuda.redditconnector.api.oauth.AuthManager;
-import com.milabuda.redditconnector.model.PostSchema;
 import com.milabuda.redditconnector.redis.RedisManager;
-import org.apache.kafka.connect.data.Struct;
+import com.milabuda.redditconnector.sourcerecord.builder.CommentRecordBuilder;
+import com.milabuda.redditconnector.sourcerecord.builder.PostRecordBuilder;
+import com.milabuda.redditconnector.sourcerecord.transformer.CommentTransformer;
+import com.milabuda.redditconnector.sourcerecord.transformer.PostTransformer;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.Jedis;
 
-import java.time.Instant;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import static com.milabuda.redditconnector.model.PostSchema.AUTHOR_FIELD;
-import static com.milabuda.redditconnector.model.PostSchema.SELF_TEXT_FIELD;
-import static com.milabuda.redditconnector.model.PostSchema.CREATED_AT_FIELD;
-import static com.milabuda.redditconnector.model.PostSchema.NUM_COMMENTS_FIELD;
-import static com.milabuda.redditconnector.model.PostSchema.SCORE_FIELD;
-import static com.milabuda.redditconnector.model.PostSchema.TITLE_FIELD;
-import static com.milabuda.redditconnector.model.PostSchema.UPVOTE_RATIO_FIELD;
+import java.util.Set;
 
 public class RedditSourceTask extends SourceTask {
-  static final Logger log = LoggerFactory.getLogger(RedditSourceTask.class);
 
-  private static final String POST_KAFKA_TOPIC = "reddit-posts";
+  private static final Logger log = LoggerFactory.getLogger(RedditSourceTask.class);
 
-  public static final String LAST_READ_POST = "last_read_post";
-  public static final String DEFAULT_FROM = "Full scan";
-  public static final String SUBREDDIT_FIELD = "subreddit";
+  private static boolean initialFullScanDone = false;
 
-  private RedditSourceConfig config;
   private AuthManager authManager;
-  private String nextPostAfter;
-  private boolean initialFullScanDone = false;
+  private RedditSourceConfig config;
+  private PostManager postManager;
+  private PostRecordBuilder postRecordBuilder;
+  private PostTransformer postTransformer;
+  private CommentManager commentManager;
+  private CommentRecordBuilder commentRecordBuilder;
+  private CommentTransformer commentTransformer;
+
+  public static void setInitialFullScanDone() {
+    RedditSourceTask.initialFullScanDone = true;
+  }
+
+  public static boolean isInitialFullScanDone() {
+      return RedditSourceTask.initialFullScanDone;
+  }
 
   @Override
   public String version() {
@@ -51,29 +53,20 @@ public class RedditSourceTask extends SourceTask {
   public void start(Map<String, String> props) {
     log.debug("Starting RedditSourceTask.");
     config = new RedditSourceConfig(props);
-    log.debug("RedditSourceTask config: {}", config);
-    initializeLastVariables();
     authManager = new AuthManager(config);
-  }
-
-  private void initializeLastVariables() {
-    Map<String, Object> lastSourceOffset;
-    lastSourceOffset = context.offsetStorageReader().offset(sourcePartition());
-    if(lastSourceOffset == null){
-      log.debug("Offset storage reader is not available, using default value: {}", DEFAULT_FROM);
-      nextPostAfter = config.getLastReadPost();
-    } else{
-      log.debug("Offset storage reader is available.");
-      Object lastReadPost = lastSourceOffset.get(LAST_READ_POST);
-      if(lastReadPost instanceof String){
-        nextPostAfter = (String) lastReadPost;
-      }
-    }
+    postRecordBuilder = new PostRecordBuilder(config);
+    postTransformer = new PostTransformer(postRecordBuilder);
+    postManager = new PostManager(config, authManager);
+    commentManager = new CommentManager(config, authManager);
+    commentRecordBuilder = new CommentRecordBuilder();
+    commentTransformer = new CommentTransformer(commentRecordBuilder);
   }
 
   @Override
   public List<SourceRecord> poll() throws InterruptedException {
-    List<SourceRecord> sourceRecords = getRedditPostsAsSourceRecords(authManager);
+    List<SourceRecord> sourceRecords = new ArrayList<>();
+    sourceRecords.addAll(getRedditPostsAsSourceRecords());
+    sourceRecords.addAll(getRedditCommentsAsSourceRecords());
     Thread.sleep(5000);
     return sourceRecords;
   }
@@ -81,103 +74,15 @@ public class RedditSourceTask extends SourceTask {
   @Override
   public void stop() {
     RedisManager.closePool();
-    //TODO: Do whatever is required to stop your task.
   }
 
-  List<SourceRecord> getRedditPostsAsSourceRecords(AuthManager authManager) {
-    PostManager postManager = new PostManager(config, authManager);
-    Listing<Post> postsResponse = retrievePosts(postManager);
-
-    try (Jedis jedis = RedisManager.getJedisResource()) {
-      return postsResponse
-              .children()
-              .stream()
-              .sorted(Comparator.comparing(e -> e.data().createdUtc()))
-              .filter(post -> !jedis.sismember("posts:stored", post.data().id()))
-              .peek(post -> jedis.sadd("posts:stored", post.data().id()))
-              .map(this::generatePostSourceRecord)
-              .toList();
-    }
+  List<SourceRecord> getRedditPostsAsSourceRecords() {
+    Listing<Post> postsResponse = postManager.retrievePosts();
+    return postTransformer.transform(postsResponse);
   }
 
-  private Listing<Post> retrievePosts(PostManager postManager) {
-    boolean eligibleForFullScan = !initialFullScanDone && config.getInitialFullScan();
-    Listing<Post> postsResponse =
-            eligibleForFullScan
-                    ? performInitialFullScan(postManager)
-                    : postManager.getPosts();
-
-    if (postsResponse == null) {
-      log.info("No posts found. Returning empty list.");
-      return Listing.empty();
-    }
-    return postsResponse;
-  }
-
-  private Listing<Post> performInitialFullScan(PostManager postManager) {
-    log.info("Initial full scan of subreddits.");
-    Listing<Post> postsResponse = Listing.empty();
-    String after = null;
-    int count = 0;
-
-    while (true) {
-      Listing<Post> pageRecords = fetchPaginatedRecords(postManager, after, count);
-
-      postsResponse = postsResponse.addAll(pageRecords.children());
-      if (pageRecords.after() == null) {
-        break;
-      }
-      after = pageRecords.after();
-      count += pageRecords.children().size();
-
-    }
-    initialFullScanDone = true;
-    return postsResponse;
-  }
-
-  private Listing<Post> fetchPaginatedRecords(PostManager postManager, String after, Integer count) {
-    return postManager.getPostsWithPagination(after, count).data();
-  }
-
-  private SourceRecord generatePostSourceRecord(Envelope<Post> post) {
-    return new SourceRecord(
-            sourcePartition(),
-            sourceOffset(post.data().id()),
-            POST_KAFKA_TOPIC,
-            null,
-            PostSchema.KEY_SCHEMA,
-            buildRecordKey(post),
-            PostSchema.VALUE_SCHEMA,
-            buildRecordValue(post),
-            Instant.ofEpochSecond(post.data().createdUtc()).toEpochMilli());
-  }
-
-    private Struct buildRecordKey(Envelope<Post> post) {
-      return new Struct(PostSchema.KEY_SCHEMA)
-              .put(PostSchema.POST_ID_FIELD, post.data().id());
-    }
-
-    private Struct buildRecordValue(Envelope<Post> post) {
-        return new Struct(PostSchema.VALUE_SCHEMA)
-                .put(TITLE_FIELD, post.data().title())
-                .put(SELF_TEXT_FIELD, post.data().selftext())
-                .put(AUTHOR_FIELD, post.data().author())
-                .put(SUBREDDIT_FIELD, post.data().subreddit())
-                .put(SCORE_FIELD, post.data().score())
-                .put(NUM_COMMENTS_FIELD, post.data().numComments())
-                .put(CREATED_AT_FIELD, post.data().createdUtc())
-                .put(UPVOTE_RATIO_FIELD, post.data().upvoteRatio());
-    }
-
-  private Map<String, String> sourcePartition() {
-    Map<String, String> map = new HashMap<>();
-    map.put(SUBREDDIT_FIELD, config.getSubreddits());
-    return map;
-  }
-
-  private Map<String, String> sourceOffset(String postId) {
-    Map<String, String> map = new HashMap<>();
-    map.put(LAST_READ_POST, postId);
-    return map;
+  List<SourceRecord> getRedditCommentsAsSourceRecords() {
+    List<Comment> commentsResponse = commentManager.retrieveComments();
+    return commentTransformer.transform(commentsResponse);
   }
 }
