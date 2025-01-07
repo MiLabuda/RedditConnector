efe7c08c447a807d63a44c41e7892a2a1a15eb3c
Michał Łabuda <mlabuda544@gmail.com>
2025-01-07 19:27:18 +0100

    Make custom error decoder simpler

--- src/main/java/com/milabuda/redditconnector/api/client/CustomErrorDecoder.java
+++ src/main/java/com/milabuda/redditconnector/api/client/CustomErrorDecoder.java
@@ -1,31 +1,22 @@
 package com.milabuda.redditconnector.api.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
 import feign.codec.ErrorDecoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.BadRequestException;
-import javax.ws.rs.ForbiddenException;
-import javax.ws.rs.NotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.Optional;
 
 public class CustomErrorDecoder implements ErrorDecoder {
-        private final ErrorDecoder errorDecoder = new Default();
-        @Override
-        public Exception decode(String methodKey, Response response) {
-            ExceptionMessage message = null;
-            try (InputStream bodyIs = response.body().asInputStream()) {
-                ObjectMapper mapper = new ObjectMapper();
-                message = mapper.readValue(bodyIs, ExceptionMessage.class);
-            } catch (IOException e) {
-                return new Exception(e.getMessage());
-            }
-            return switch (response.status()) {
-                case 400 -> new BadRequestException(message.getMessage() != null ? message.getMessage() : "Bad Request");
-                case 403 -> new ForbiddenException(message.getMessage() != null ? message.getMessage() : "Forbidden");
-                case 404 -> new NotFoundException(message.getMessage() != null ? message.getMessage() : "Not found");
-                default -> errorDecoder.decode(methodKey, response);
-            };
-        }
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomErrorDecoder.class);
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        String errorMessage = String.format("Feign Client Error - Method: %s, Status: %d, Reason: %s",
+                methodKey, response.status(), Optional.ofNullable(response.reason()).orElse("Unknown"));
+
+        logger.error(errorMessage);
+        return new RuntimeException(errorMessage);
     }
+}
